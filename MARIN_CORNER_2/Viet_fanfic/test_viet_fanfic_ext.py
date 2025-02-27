08f77778acbfc738094acadb03eafcb0823a92ac
YangXing0810 <baotranngopham0810@gmail.com>
2025-02-27 20:11:10 +0700

    Tran

--- MARIN_CORNER_2/Viet_fanfic/test_viet_fanfic_ext.py
+++ MARIN_CORNER_2/Viet_fanfic/test_viet_fanfic_ext.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication
 
-from Group_Project.Viet_fanfic_ext import Viet_fanfic_ext
+from MARIN_CORNER_2.Viet_fanfic.Viet_fanfic_ext import Viet_fanfic_ext
 
 app=QApplication([])
 Mainwindow=QMainWindow()
